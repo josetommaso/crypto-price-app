803b7cb27b7ddd2d6ea32dc5542445b6482c440e
Jose Tommaso <tommaso_j@hotmail.com>
2021-04-06 00:19:41 +0100

    styles for the price component

--- src/components/Price.js
+++ src/components/Price.js
@@ -1,14 +1,32 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const ResultDiv = styled.div`
+    color: #fff;
+    font-family: Arial, Helvetica, sans-serif;
+    span {
+        font-weight: bold;
+    }
+`; 
+
+const Info = styled.p`
+    font-size: 18px;
+`;
+
+const PriceParagraph = styled.p`
+    font-size: 1.9rem;
+    
+`;
 
 const Price = ({result}) => {
     if(Object.keys(result).length === 0) return null
     return ( 
-        <div>
-            <p>Price: <span>{result.PRICE}</span></p>
-            <p>Today's highest price: <span>{result.HIGHDAY}</span></p>
-            <p>Today's lowest price: <span>{result.LOWDAY}</span></p>
-            <p>Last Update: <span>{result.LASTUPDATE}</span></p>
-        </div>
+        <ResultDiv>
+            <PriceParagraph>Price: <span>{result.PRICE}</span></PriceParagraph>
+            <Info>Today's highest price: <span>{result.HIGHDAY}</span></Info>
+            <Info>Today's lowest price: <span>{result.LOWDAY}</span></Info>
+            <Info>Last Update: <span>{result.LASTUPDATE}</span></Info>
+        </ResultDiv>
      );
 }
  
